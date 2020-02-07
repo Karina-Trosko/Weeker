eb4bef99184239d3c70f9cb76ba6444011d71ccd
Karina Trosko <karyna_trasko@epam.com>
2020-02-07 17:31:41 +0300

    Splash screen with a white monemt

--- android/app/src/main/java/com/awesomeproject1/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject1/MainApplication.java
@@ -3,6 +3,7 @@ package com.awesomeproject1;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SplashScreenReactPackage(),
             new AsyncStoragePackage(),
             new VectorIconsPackage()
       );
@@ -43,7 +45,7 @@ public class MainApplication extends Application implements ReactApplication {
 
   @Override
   public void onCreate() {
-//    setTheme(R.style.AppTheme);
+  //  setTheme(R.style.AppTheme);
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
   }
